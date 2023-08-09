71cb336542db67f10520dc63218e696533675329
jd-58 <138058948+jd-58@users.noreply.github.com>
2023-08-09 14:43:45 -0400

    Added game function
    The game function is added. No loop is currently used - future iteration will use loops and update game win condition logic to cut down on repetitive if else statements

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ function game() {
             
         }
         console.log(playRound())
+        console.log(playRound())
+        console.log(playRound())
+        console.log(playRound())
+        console.log(playRound())
 
 
         
